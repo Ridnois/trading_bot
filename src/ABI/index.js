b01c6f766647399bd4ba68d9353433b8335595b6
Sebastian Altamirano <ridnois@protonmail.com>
2022-01-24 17:20:24 -0300

    feat/ts: Getting pair contractw

--- src/ABI/index.js
+++
@@ -1,3 +0,0 @@
-export pair_abi from './pair_abi.json'
-export wbnb_abi from './wbnb_abi.json'
-export factory_abi from './factory_abi'
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,40 @@
 import * as dotenv from 'dotenv'
 import Web3 from 'web3'
 import { AbiItem } from 'web3-utils'
-import { pancake_factory_abi } from './ABI'
+import ABI from './ABI'
 
 dotenv.config()
 
-const { PANCAKE_FACTORY } = process.env
-
-const web3Handler = (rcp: string) => new Web3(rcp)
-
-const contract = (web3Handler: Web3) => (abi: AbiItem[], address: string | undefined) => new web3Handler.eth.Contract(abi, address)
-
-const binance = web3Handler('https://bsc-dataseed1.binance.org:443/')
-const binanceContract = contract(binance)
-const pancakeFactory = binanceContract(pancake_factory_abi, PANCAKE_FACTORY)
-
-const pairAddressHandler = (pairFactory: any) => async (token0: string, token1: string) => await pairFactory.methods.getPair(token0, token1).call()
 
 
+const { PANCAKE_FACTORY,
+	PANCAKE_ROUTER,
+	WBNB_ADDRESS,
+	BUSD_ADDRESS,
+	} = process.env
+
+type fromEnv = string | undefined
+
+const rcpHandler = (rcp: string) => new Web3(rcp)
+
+const contractFactory = (rcpHandler: Web3) => (abi: AbiItem[], address: fromEnv) => new rcpHandler.eth.Contract(abi, address)
+
+const pairAddressHandler = (pairFactory: any) => async (token0: fromEnv, token1: fromEnv) => { 
+	return await pairFactory.methods.getPair(token0, token1).call()
+}
+
+
+const initBinance = async () => {
+	const binance = rcpHandler('https://bsc-dataseed1.binance.org:443')
+	const binanceContract = contractFactory(binance)
+	const pairFactoryContract = binanceContract(ABI.factory_abi as AbiItem[], PANCAKE_FACTORY)
+	const pairAddress = pairAddressHandler(pairFactoryContract)
+
+	const myPairAddress = await pairAddress(WBNB_ADDRESS, BUSD_ADDRESS)
+	const myPair = binanceContract(ABI.pair_abi as AbiItem[], myPairAddress)
+
+	console.log(myPair)
+}
+
+
+initBinance()
